c6ac10832a7f1a934af85c780778930abc378e16
DenysFerreira <lerroandeco@gmail.com>
2020-07-01 22:25:54 -0300

    REST API Config. for Read Only (Release 1.0)

---
+++ 02-back-end/spring-boot-ecommerce/src/main/java/com/dlerroan/ecommerce/config/MyDataRestConfig.java
@@ -0,0 +1,32 @@
+package com.dlerroan.ecommerce.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+
+import com.dlerroan.ecommerce.entity.Product;
+import com.dlerroan.ecommerce.entity.ProductCategory;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer{
+
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+
+		HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+		
+		// disable HTTP methods for Product: PUT, POST and DELETE
+		config.getExposureConfiguration()
+				.forDomainType(Product.class)
+				.withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+				.withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+		
+		// disable HTTP methods for ProductCategory: PUT, POST and DELETE
+		config.getExposureConfiguration()
+				.forDomainType(ProductCategory.class)
+				.withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+				.withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+		
+	}
+}
