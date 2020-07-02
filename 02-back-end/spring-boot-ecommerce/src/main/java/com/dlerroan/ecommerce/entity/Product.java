32f1c2480fa360e7a0699a2c71458f5f4aa438ce
DenysFerreira <lerroandeco@gmail.com>
2020-07-01 21:50:34 -0300

    Bug fix Lombok: Getters and Setters methods were added

--- 02-back-end/spring-boot-ecommerce/src/main/java/com/dlerroan/ecommerce/entity/Product.java
+++ 02-back-end/spring-boot-ecommerce/src/main/java/com/dlerroan/ecommerce/entity/Product.java
@@ -52,5 +52,93 @@ public class Product {
 	@Column(name = "last_updated")
 	@UpdateTimestamp
 	private Date lastUpdated;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public ProductCategory getCategory() {
+		return category;
+	}
+
+	public void setCategory(ProductCategory category) {
+		this.category = category;
+	}
+
+	public String getSku() {
+		return sku;
+	}
+
+	public void setSku(String sku) {
+		this.sku = sku;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public BigDecimal getUnitPrice() {
+		return unitPrice;
+	}
+
+	public void setUnitPrice(BigDecimal unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	public Integer getUnitsInStock() {
+		return unitsInStock;
+	}
+
+	public void setUnitsInStock(Integer unitsInStock) {
+		this.unitsInStock = unitsInStock;
+	}
+
+	public Date getDateCreated() {
+		return dateCreated;
+	}
+
+	public void setDateCreated(Date dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+
+	public Date getLastUpdated() {
+		return lastUpdated;
+	}
+
+	public void setLastUpdated(Date lastUpdated) {
+		this.lastUpdated = lastUpdated;
+	}
 	
 }
--- 02-back-end/spring-boot-ecommerce/src/main/java/com/dlerroan/ecommerce/entity/ProductCategory.java
+++ 02-back-end/spring-boot-ecommerce/src/main/java/com/dlerroan/ecommerce/entity/ProductCategory.java
@@ -32,4 +32,28 @@ public class ProductCategory {
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
 	private Set<Product> products;
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+
+	public Set<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(Set<Product> products) {
+		this.products = products;
+	}
+
 }
