09c840e33599a2fcf54e135f00759fc79d956b6b
DenysFerreira <lerroandeco@gmail.com>
2020-07-02 22:00:50 -0300

    Building an HTML table

--- 02-back-end/spring-boot-ecommerce/src/main/java/com/dlerroan/ecommerce/dao/ProductCategoryRepository.java
+++ 02-back-end/spring-boot-ecommerce/src/main/java/com/dlerroan/ecommerce/dao/ProductCategoryRepository.java
@@ -2,9 +2,11 @@ package com.dlerroan.ecommerce.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.dlerroan.ecommerce.entity.ProductCategory;
 
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long>{
 
--- 02-back-end/spring-boot-ecommerce/src/main/java/com/dlerroan/ecommerce/dao/ProductRepository.java
+++ 02-back-end/spring-boot-ecommerce/src/main/java/com/dlerroan/ecommerce/dao/ProductRepository.java
@@ -1,10 +1,11 @@
 package com.dlerroan.ecommerce.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.dlerroan.ecommerce.entity.Product;
 
-
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long>{
 
 }
